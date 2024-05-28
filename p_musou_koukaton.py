bde785720da3b40ba58bd161fe0fde90efea9c47
tbt <c0a2310239@edu.teu.ac.jp>
2024-05-28 14:44:39 +0900

    一旦完成

--- p_musou_koukaton.py
+++ p_musou_koukaton.py
@@ -324,7 +324,20 @@ class Sheeld(pg.sprite.Sprite):
         if self.life < 0:
             __class__.is_not_shield = True
             self.kill()
-        
+
+
+class Bonus(pg.sprite.Sprite):
+    """
+    ボーナスアイテムを出現させるクラス
+    アイテムに触れるとスコア+100
+    """
+
+    def __init__(self,x:tuple):
+        super().__init__()
+        self.image = pg.image.load(f"fig/small_star7_yellow.png")    #画像Surfaceの定義
+        self.image=pg.transform.rotozoom(self.image,0,0.18)   #画像の大きさ調整
+        self.rect = self.image.get_rect()
+        self.rect.center = x
 
         
 
@@ -335,7 +348,6 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load(f"fig/pg_bg.jpg")
     score = Score()
-    #neobeam = NeoBeam 
     #score.value =900000000
 
     bird = Bird(3, (900, 400))
@@ -345,6 +357,7 @@ def main():
     emys = pg.sprite.Group()
     gravity = pg.sprite.Group()
     sheelds = pg.sprite.Group()
+    bonus = pg.sprite.Group()
 
     tmr = 0
     clock = pg.time.Clock()
@@ -389,8 +402,12 @@ def main():
                 bombs.add(Bomb(emy, bird))
 
         for emy in pg.sprite.groupcollide(emys, beams, True, True).keys():
-            exps.add(Explosion(emy, 100))  # 爆発エフェクト
+            if random.randint(0,10) == 5:        # 10分1の確率で
+                bonus.add(Bonus(emy.rect.center))    #ボーナスアイテムを描画する
+            else:
+                exps.add(Explosion(emy, 100))  # 爆発エフェクト
             score.value += 10  # 10点アップ
+            
             bird.change_img(6, screen)  # こうかとん喜びエフェクト
 
         for bomb in pg.sprite.groupcollide(bombs, beams, True, True).keys():
@@ -405,7 +422,11 @@ def main():
         for bomb in pg.sprite.groupcollide(bombs, gravity, True, False).keys():
             exps.add(Explosion(bomb, 50))
             score.value += 1
+        bonus_item=pg.sprite.spritecollide(bird,bonus, False)
 
+        if len(bonus_item) != 0 :    # ボーナスアイテムとこうかとんが衝突したら
+            score.value += 100       # スコア+100
+            bonus_item[0].kill()     # ボーナスアイテムを消す
         
         
         # Cボタンを押すとシールドを展開
@@ -451,6 +472,8 @@ def main():
         score.update(screen)
         gravity.draw(screen)
         gravity.update()
+        if len(bonus) != 0:
+            bonus.draw(screen)   # ボーナスアイテムの描画
 
         if not Sheeld.is_not_shield:
             sheelds.update(bird)
